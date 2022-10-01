21d26cc5db89ccfe60a124080da21cc02284c50c
Randy Le <randyL04@hey.com>
2022-10-01 08:17:37 -0700

    update to new proto file name

--- proto/test.py
+++ proto/test.py
@@ -2,28 +2,27 @@ from email import message
 import logging
 
 import grpc
-import cdata_pb2
-import cdata_pb2_grpc
-
+import rcdp_pb2
+import rcdp_pb2_grpc
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = cdata_pb2_grpc.SQLClientStub(channel)
+        stub = rcdp_pb2_grpc.SQLClientStub(channel)
         response = stub.IsOnline(
-            cdata_pb2.TestRequest(requestEchoMessage='test1'))
+            rcdp_pb2.TestRequest(requestEchoMessage='test1'))
     print("Greeter client received: " + response.replyEchoMessage)
 
 
 def test_create_db():
-    auth = cdata_pb2.AuthRequest(userName="tester", pw="123456")
-    request = cdata_pb2.CreateUserDatabaseRequest(
+    auth = rcdp_pb2.AuthRequest(userName="tester", pw="123456")
+    request = rcdp_pb2.CreateUserDatabaseRequest(
         authentication=auth, databaseName="test_create_db.db")
 
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = cdata_pb2_grpc.SQLClientStub(channel)
+        stub = rcdp_pb2_grpc.SQLClientStub(channel)
         response = stub.CreateUserDatabase(request)
     print("Greeter client received: " + str(response.isCreated))
 
@@ -36,4 +35,4 @@ if __name__ == '__main__':
 # https://grpc.io/docs/languages/python/basics/
 # https://realpython.com/python-microservices-grpc/
 # run this from the "proto" directory
-# python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. cdata.proto
+# python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. rcdp.proto
